f6e635fa58f52ded7d887d8d933d76d74f1a0cc7
Ivan <ivanc11235@gmail.com>
2021-12-05 21:04:53 -0500

    implemented the filtering function. also added some docstrings

--- Detectron_roadside.py
+++ Detectron_roadside.py
@@ -1,3 +1,4 @@
+import detectron2
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog
@@ -15,7 +16,7 @@ class Detector:
     def __init__(self):
         self.cfg = get_cfg()
 
-        # Load model config and pretrained model
+        # Load model config and model weights
         self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         self.cfg.MODEL.DEVICE = "cuda"
@@ -36,63 +37,58 @@ class Detector:
         self.interested_classes_num = self.construct_interested_cls_num()
 
 
-    # def construct_class_dict(self):
-    #     for i, name in enumerate(MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).thing_classes):
-    #         self.classes_dict[name] = i
+    def construct_class_dict(self):
+        for i, name in enumerate(MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).thing_classes):
+            self.classes_dict[name] = i
         
-    # def construct_interested_cls_num(self):
-    #     intr_cls_num = []
-    #     for name in self.interested_classes:
-    #         num = self.classes_dict[name]
-    #         intr_cls_num.append(num)
-    #     return intr_cls_num
+    def construct_interested_cls_num(self):
+        intr_cls_num = []
+        for name in self.interested_classes:
+            num = self.classes_dict[name]
+            intr_cls_num.append(num)
+        return intr_cls_num
 
 
 
-    # def filtered_outputs(self, imagePath):
-    #     image = cv2.imread(imagePath)
-    #     # import pdb
-    #     # pdb.set_trace()
-    #     t_image = image[np.newaxis, :]
-    #     print(t_image.shape)
-    #     outputs = self.predictor(t_image)
-    #     # outputs = self.predictor(image)
-    #     pred_classes = outputs["instances"].pred_classes
-    #     pred_boxes = outputs["instances"].pred_boxes
-    #     # print(pred_classes)
-    #     # print(pred_boxes)
+    def filtered_outputs(self, pred_classes, pred_boxes):
+        pred_classes_list = pred_classes.tolist()
 
-    #     pred_classes_list = pred_classes.tolist()
+        indx_to_remove = []
+        for i, num in enumerate(pred_classes_list):
+            if num not in self.interested_classes_num:
+                indx_to_remove.append(i)
 
-    #     indx_to_remove = []
-    #     for i, num in enumerate(pred_classes_list):
-    #         if num not in self.interested_classes_num:
-    #             indx_to_remove.append(i)
-    #     # print(indx_to_remove)
-
-    #     pred_classes = np.delete(pred_classes.cpu().numpy(), indx_to_remove)
-    #     pred_boxes = np.delete(pred_boxes.tensor.cpu().numpy(), indx_to_remove, axis=0)
-    #     # print(pred_classes)
-    #     # print(pred_boxes)
-
-    #     for data in pred_classes:
-    #         num = data.item()
-    #         print(MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).thing_classes[num])
+        # convert to numpy to use np.delete
+        pred_classes = np.delete(pred_classes.cpu().detach().numpy(), indx_to_remove)
+        pred_boxes = np.delete(pred_boxes.cpu().detach().numpy(), indx_to_remove, axis=0)
         
-    #     return pred_classes, pred_boxes
+        # convert back to tensor
+        pred_classes = torch.tensor(pred_classes).cuda()
+        pred_boxes = torch.tensor(pred_boxes).cuda()
+        
+        # # Print outs for debugging
+        # print(indx_to_remove)
+        # print(pred_classes)
+        # print(pred_boxes)
+        # for data in pred_classes:
+        #     num = data.item()
+            # print(MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).thing_classes[num])
+        
+        return pred_classes, pred_boxes
 
 
 
-    def single_inference(self, image_path):
+    def single_inference(self, image_path, filter=False):
         """Takes in a path to an image and returns tensors for the classes (indices)
-        in the image, and a tensor for the bounding boxes.
+            in the image, and a tensor for the bounding boxes.
 
         Args:
             image_path (string): path to a single images
+            filter (boolean): whether to filter out unwanted classes
 
         Returns:
-            pred_classes: tensor with a list of the identified classes
-            pred_boxes: tensor with the bounding box coordinates (top left and bottom right)
+            pred_classes: tensor with the indices of identified classes
+            pred_boxes: tensor with the bounding box coordinates (top left and bottom right) for each instance identified
         """
 
         img = cv2.imread(image_path)
@@ -102,14 +98,29 @@ class Detector:
 
         outputs = self.model(inputs)
 
-        pred_classes = outputs[0]["instances"].pred_classes
-        pred_boxes = outputs[0]["instances"].pred_boxes.tensor
-
+        pred_classes = outputs[0]["instances"].pred_classes.detach()
+        pred_boxes = outputs[0]["instances"].pred_boxes.tensor.detach()
+        
+        if filter:
+            pred_classes, pred_boxes = self.filtered_outputs(pred_classes, pred_boxes)
 
         return pred_classes, pred_boxes
 
     
-    def batch_inference_frompaths(self, list_of_paths):
+    def batch_inference_frompaths(self, list_of_paths, filter=False):
+        """Takes in a list of paths to images and returns a list of tensors for the classes (indices)
+            in the images, and a list of tensors for the bounding boxes.
+
+        Args:
+            image_path (list of string): list of paths to images
+            filter (boolean): whether to filter out unwanted classes
+
+        Returns:
+            list_of_pred_classes: list of tensors with the indices of identified classes.
+                                If filter is True, then it is possible each of the tensors will be completely empty.
+            list_of_pred_boxes: list of tensors with the bounding box coordinates (top left and bottom right) for each instance identified
+                                If filter is True, then it is possible each of the tensors will be completely empty.
+        """
         inputs = []
         for image_path in list_of_paths:
             img = cv2.imread(image_path)
@@ -122,8 +133,11 @@ class Detector:
         list_of_pred_classes = []
         list_of_pred_boxes = []
         for i in range(0, len(outputs)):
-            pred_classes = outputs[i]["instances"].pred_classes
-            pred_boxes = outputs[i]["instances"].pred_boxes.tensor
+            pred_classes = outputs[i]["instances"].pred_classes.detach()
+            pred_boxes = outputs[i]["instances"].pred_boxes.tensor.detach()
+            
+            if filter:
+                pred_classes, pred_boxes = self.filtered_outputs(pred_classes, pred_boxes)
 
             list_of_pred_classes.append(pred_classes)
             list_of_pred_boxes.append(pred_boxes)
@@ -132,7 +146,21 @@ class Detector:
         return list_of_pred_classes, list_of_pred_boxes
 
 
-    def batch_inference_fromtensors(self, stacked_tensors):
+    def batch_inference_fromtensors(self, stacked_tensors, filter=False):
+        """Takes in a batched tensor of images of shape (batch_size, C, H, W) 
+            and returns a list of tensors for the classes (indices)
+            in the images, and a list of tensors for the bounding boxes.
+
+        Args:
+            stacked_tensors (tensor): tensor of shape (batch_size, C, H, W) that contains images (e.g. from a dataloader)
+            filter (boolean): whether to filter out unwanted classes
+
+        Returns:
+            list_of_pred_classes: list of tensors with the indices of identified classes.
+                                If filter is True, then it is possible each of the tensors will be completely empty.
+            list_of_pred_boxes: list of tensors with the bounding box coordinates (top left and bottom right) for each instance identified
+                                If filter is True, then it is possible each of the tensors will be completely empty.
+        """
         batch_size = stacked_tensors.size()[0]
         inputs = []
         for i in range(0, batch_size):
@@ -145,8 +173,11 @@ class Detector:
         list_of_pred_classes = []
         list_of_pred_boxes = []
         for i in range(0, len(outputs)):
-            pred_classes = outputs[i]["instances"].pred_classes
-            pred_boxes = outputs[i]["instances"].pred_boxes.tensor
+            pred_classes = outputs[i]["instances"].pred_classes.detach()
+            pred_boxes = outputs[i]["instances"].pred_boxes.tensor.detach()
+            
+            if filter:
+                pred_classes, pred_boxes = self.filtered_outputs(pred_classes, pred_boxes)
 
             list_of_pred_classes.append(pred_classes)
             list_of_pred_boxes.append(pred_boxes)
--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from Detectron_roadside import *
 
 detector = Detector()
 
-image_path = "example_images/dogs1.png"
+# image_path = "example_images/dogs1.png"  # all classes will be filtered out
+image_path = "example_images/cityscapes1.png"  # no classes will be filtered out
 
 
 
 
-# Single inference
-pred_classes, pred_boxes = detector.single_inference(image_path)
-print(pred_classes)
-print(pred_boxes)
+# # Single inference
+# pred_classes, pred_boxes = detector.single_inference(image_path, filter=True)
+# print(pred_classes)
+# print(pred_boxes)
 
 
-# Batch inference given multiple image paths
-list_of_image_paths = [image_path, image_path, image_path]
-list_pred_classes, list_pred_boxes = detector.batch_inference_frompaths(list_of_image_paths)
-print(list_pred_classes)
-print(list_pred_boxes)
+# # Batch inference given multiple image paths
+# list_of_image_paths = [image_path, image_path, image_path]
+# list_pred_classes, list_pred_boxes = detector.batch_inference_frompaths(list_of_image_paths, filter=True)
+# print(list_pred_classes)
+# print(list_pred_boxes)
 
 
 # Batch inference given batched tensor
@@ -26,6 +27,6 @@ img = np.transpose(img,(2,0,1))
 img_tensor = torch.from_numpy(img)
 images_tensors = torch.stack([img_tensor, img_tensor, img_tensor])
 
-list_pred_classes, list_pred_boxes = detector.batch_inference_fromtensors(images_tensors)
+list_pred_classes, list_pred_boxes = detector.batch_inference_fromtensors(images_tensors, filter=True)
 print(list_pred_classes)
 print(list_pred_boxes)
